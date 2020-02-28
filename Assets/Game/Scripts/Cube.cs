bf8cd3299627ab8c8f76f71771f3c8dfb3c6a628
liaozhouyu <liaozhouyu@nibirutech.com>
2020-02-28 19:18:43 +0800

    add

---
+++ Assets/Game/Scripts/Cube.cs
@@ -0,0 +1,27 @@
+﻿using SonicBloom.Koreo;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField]
+    private string EventId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEvents(EventId, Jump);
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Jump(KoreographyEvent koreoEvent)
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up, ForceMode.Impulse);
+    }
+}
